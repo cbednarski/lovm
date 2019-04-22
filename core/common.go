ee5db6b8c0a9775cbd53ef4359a4db106ca97224
Chris Bednarski <chris@cbednarski.com>
2019-04-21 23:14:41 -0700

    Added docs for ErrNotImplemented

--- core/common.go
+++ core/common.go
@@ -8,7 +8,12 @@ import (
 	"strings"
 )
 
-var ErrNotImplemented = errors.New("not implemented")
+var (
+	// ErrNotImplemented is returned as a placeholder when a particular feature
+	// is not yet implemented. This allows any part of the implementation to
+	// satisfy interface requirements before a full implementation is available.
+	ErrNotImplemented = errors.New("not implemented (TODO)")
+)
 
 func CommandError(command *exec.Cmd, output []byte) {
 	os.Stderr.WriteString(fmt.Sprintf("[command debug] %s\n", strings.Join(command.Args, " ")))
